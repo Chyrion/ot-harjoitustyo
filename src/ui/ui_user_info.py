88468d7a78cc36c6350291abf98562cfaa4a0114
Chyrion <laine.vili@gmail.com>
2023-04-25 20:07:43 +0300

    Extend amount of info displayed, add placeholder for plane list

--- src/ui/ui_user_info.py
+++ src/ui/ui_user_info.py
@@ -25,8 +25,7 @@ class UIUserInfo:
         return self._user.username
 
     def _initialize(self):
-        print(self._user)
-        self._frame = ttk.Frame(master=self._root)
+        self._frame = ttk.Frame(self._root)
         title_label = ttk.Label(self._frame, text='User info', font=(
             'Arial', 16)).grid(row=0, column=0, columnspan=2)
 
@@ -34,10 +33,24 @@ class UIUserInfo:
             self._frame, text='Return', command=self._handle_show_homepage).grid(row=0, column=2)
 
         name_label = ttk.Label(self._frame, text=self._user.username, font=(
-            'Arial', 14)).grid(row=1, column=0, columnspan=2)
+            'Arial', 12)).grid(row=1, column=0, columnspan=2)
+
         flights_text = ttk.Label(
             self._frame, text='Flights:').grid(row=2, column=0)
         flights_number = ttk.Label(self._frame, text=len(
             self._user.flights)).grid(row=2, column=1)
 
+        hours_text = ttk.Label(
+            self._frame, text='Total hours:').grid(row=3, column=0)
+        hours_number = ttk.Label(
+            self._frame, text=self._user.hours).grid(row=3, column=1)
+
+        planes_label = ttk.Label(self._frame, text='Planes:', font=(
+            'Arial', 12)).grid(row=4, column=0, columnspan=2)
+        self._initialize_planes_list()
+
         self.pack()
+
+    def _initialize_planes_list(self):
+        placeholder_label = ttk.Label(
+            self._frame, text='Placeholder for plane list').grid(row=5, column=0)
