fa2a74bc67bbbdc0273fe50662f8b5fe25b10b1a
Chyrion <laine.vili@gmail.com>
2023-05-02 22:43:32 +0300

    Docstring

--- src/entities/flightplan.py
+++ src/entities/flightplan.py
@@ -2,6 +2,14 @@ class FlightPlan:
     '''Class representing a flight plan'''
 
     def __init__(self, start: str, destination: str):
+        '''Class constructor
+
+        args:
+            start:
+                The starting airport for the flight plan
+            destination:
+                The destination airport of the flight plan
+        '''
         self._start = start
         self._destination = destination
 
@@ -14,4 +22,5 @@ class FlightPlan:
         return self._destination
 
     def text_representation(self):
+        '''Returns a text representation of the flight plan'''
         return f'{self.start}-{self.destination}'
