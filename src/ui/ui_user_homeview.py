793419322f106d4f834f2a4065bf14767b5e5855
Chyrion <laine.vili@gmail.com>
2023-05-02 22:06:23 +0300

    Implement listing flight plans in home view

--- src/ui/ui_user_homeview.py
+++ src/ui/ui_user_homeview.py
@@ -15,6 +15,8 @@ class UIUserHomeview:
         self._handle_user_info = handle_user_info
         self._handle_new_flightplan = handle_new_flightplan
 
+        self._curr_row = 1
+
         self._initialize()
 
     def pack(self):
@@ -44,6 +46,7 @@ class UIUserHomeview:
         ).grid(column=4, row=0)
 
         self._initialize_flight_log()
+        self._initialize_flightplans()
 
         self.pack()
 
@@ -52,7 +55,6 @@ class UIUserHomeview:
 
         if len(self._user.flights) != 0:
             flights = self._user.flights
-            curr_row = 1
             for flight in flights:
                 flight_date = datetime.datetime.fromisoformat(
                     str(flight.flight_date))
@@ -60,28 +62,64 @@ class UIUserHomeview:
                     master=self._frame, text='Flight', font=("Arial", 16)).grid(column=0, columnspan=4)
 
                 label_date_text = ttk.Label(master=self._frame, text='Date').grid(
-                    column=0, row=curr_row+1, sticky=constants.N)
+                    column=0, row=self._curr_row+1, sticky=constants.N)
                 label_start_text = ttk.Label(
-                    master=self._frame, text='Start').grid(column=1, row=curr_row+1, sticky=constants.N)
+                    master=self._frame, text='Start').grid(column=1, row=self._curr_row+1, sticky=constants.N)
                 label_destination_text = ttk.Label(
-                    master=self._frame, text='Destination').grid(column=2, row=curr_row+1, sticky=constants.N)
+                    master=self._frame, text='Destination').grid(column=2, row=self._curr_row+1, sticky=constants.N)
                 label_duration_text = ttk.Label(master=self._frame, text='Duration').grid(
-                    column=3, row=curr_row+1, sticky=constants.N)
+                    column=3, row=self._curr_row+1, sticky=constants.N)
 
                 label_date = ttk.Label(
-                    master=self._frame, text=f'{flight_date.day} {flight_date.strftime("%B")} {flight_date.year}').grid(column=0, row=curr_row+2, sticky=constants.N)
+                    master=self._frame, text=f'{flight_date.day} {flight_date.strftime("%B")} {flight_date.year}').grid(column=0, row=self._curr_row+2, sticky=constants.N)
 
                 label_start_location = ttk.Label(
-                    master=self._frame, text=flight.start).grid(column=1, row=curr_row+2, sticky=constants.N)
+                    master=self._frame, text=flight.start).grid(column=1, row=self._curr_row+2, sticky=constants.N)
 
                 label_destination_location = ttk.Label(
-                    master=self._frame, text=flight.destination).grid(column=2, row=curr_row+2, sticky=constants.N)
+                    master=self._frame, text=flight.destination).grid(column=2, row=self._curr_row+2, sticky=constants.N)
 
                 label_duration = ttk.Label(master=self._frame, text=f'{flight.duration} h').grid(
-                    column=3, row=curr_row+2, sticky=constants.N)
+                    column=3, row=self._curr_row+2, sticky=constants.N)
 
-                curr_row += 4
+                label_plane_text = ttk.Label(self._frame, text='Plane')
+                label_plane_text.grid(
+                    column=4, row=self._curr_row+1, sticky=constants.N)
+
+                label_plane = ttk.Label(
+                    self._frame, text=flight.plane.tailnumber)
+                label_plane.grid(
+                    column=4, row=self._curr_row+2, sticky=constants.N)
+
+                self._curr_row += 4
 
         else:
             label = ttk.Label(master=self._frame, text='No flights found')
             label.grid(row=1, column=0)
+
+    def _initialize_flightplans(self):
+        if len(self._user.flightplans) != 0:
+            for plan in self._user.flightplans:
+                label_title = ttk.Label(
+                    master=self._frame, text='Flight plan', font=("Arial", 16))
+                label_title.grid(column=0, row=self._curr_row+1, columnspan=4)
+
+                start_label_text = ttk.Label(self._frame, text='Start')
+                start_label_text.grid(
+                    column=0, columnspan=2, row=self._curr_row+2)
+
+                dest_label_text = ttk.Label(self._frame, text='Start')
+                dest_label_text.grid(
+                    column=2, columnspan=2, row=self._curr_row+2)
+
+                start_label = ttk.Label(self._frame, text=plan.start)
+                start_label.grid(column=0, columnspan=2, row=self._curr_row+3)
+
+                dest_label = ttk.Label(self._frame, text=plan.destination)
+                dest_label.grid(column=2, columnspan=2, row=self._curr_row+3)
+
+                self._curr_row += 4
+
+        else:
+            label = ttk.Label(self._frame, text='No flight plans found')
+            label.grid(column=0)
